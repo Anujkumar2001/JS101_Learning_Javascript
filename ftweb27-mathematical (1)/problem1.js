dba2d9b7a420377fd30741c844f6e39ff4603e6c
anuj <techplaza22@gmail.com>
2023-04-16 08:41:54 +0530

    day2

---
+++ ftweb27-mathematical (1)/problem1.js
@@ -0,0 +1,14 @@
+let name= "luffy"; //name is variable.
+// we declared a variable name and assigned a value luffy.
+
+console.log(name);
+
+
+let fathername; // declaring a variable.
+fathername="dragon"; //assigning a value to it.
+
+fathername= "hi"; //re-assigning
+
+console.log(fathername);
+
+
